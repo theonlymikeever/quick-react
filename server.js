b6fc98e0c3636c2265cb6e92b5dbf05b10118698
Mike Kerslake <theonlymikeever@gmail.com>
2017-11-21 11:58:54 -0500

    First build

---
+++ server.js
@@ -0,0 +1,17 @@
+//Requires
+const express = require('express');
+const app = express();
+const path = require('path');
+const chalk = require('chalk');
+const morgan = require('morgan');
+
+//Static Routes
+app.use('/dist', express.static(path.join(__dirname, 'dist')));
+app.use(morgan('dev')) // logging
+
+//Main App Route
+app.get('/', (req, res, next) => res.sendFile(path.join(__dirname, 'index.html')));
+
+//Run Server
+const port = 1337;
+app.listen(process.env.PORT || 3000, () => console.log(chalk.blue(`Listening intently on port ${port}`)));
