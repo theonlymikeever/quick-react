877074d3b3bf7cfc0d094ccba4e4db01b1fc8a62
Mike Kerslake <theonlymikeever@gmail.com>
2017-11-21 13:14:33 -0500

    hello world!

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Main from './Main';
 
-ReactDOM.render(<hr />, document.getElementById('root'));
+ReactDOM.render(<Main />, document.getElementById('root'));
